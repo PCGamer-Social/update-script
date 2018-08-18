648cce3dfe61ac8511d73c6f778ba27b3191ae1c
eai04191 <eai@mizle.net>
2018-08-19 00:30:07 +0900

    Add unlisted option

--- update.sh
+++ update.sh
@@ -2,14 +2,14 @@
 
 COMMITHASH=$(git ls-remote https://github.com/PCGamer-Social/mastodon.git HEAD | head -c 7)
 cd ~/mastodon
-echo "[${COMMITHASH}] 📢 Enrichment Center より最新版のアップデートを開始します。Enrichment Center スタッフが最善の努力を行っていますが、重大な事故が発生する可能性があることを覚えておいてください。" | toot
+echo "[${COMMITHASH}] 📢 Enrichment Center より最新版のアップデートを開始します。Enrichment Center スタッフが最善の努力を行っていますが、重大な事故が発生する可能性があることを覚えておいてください。" | toot --visibility unlisted
 git pull
-echo "[${COMMITHASH}] 🗜️ データの取得が完了しました。ビルドを開始します。" | toot
+echo "[${COMMITHASH}] 🗜️ データの取得が完了しました。ビルドを開始します。" | toot --visibility unlisted
 docker-compose build
 # docker-compose run --rm web rails db:migrate
-# echo "[${COMMITHASH}] ぷりこんぱいる？" | toot
+# echo "[${COMMITHASH}] ぷりこんぱいる？" | toot --visibility unlisted
 # docker-compose run --rm web rails assets:precompile
-echo "[${COMMITHASH}] 🔜 デプロイを開始します。利用可能になるまで、あと、3 秒... 2 秒... 1 秒..." | toot
+echo "[${COMMITHASH}] 🔜 デプロイを開始します。利用可能になるまで、あと、3 秒... 2 秒... 1 秒..." | toot --visibility unlisted
 docker-compose up -d
 
 while true; do
@@ -22,5 +22,5 @@ while true; do
                 sudo systemctl restart nginx
 done
 
-echo "[${COMMITHASH}] ✅ アップデートが完了しました。Aperture Science の Mastodon ソーシャルテストにご協力いただき、ありがとうございます。" | toot
+echo "[${COMMITHASH}] ✅ アップデートが完了しました。Aperture Science の Mastodon ソーシャルテストにご協力いただき、ありがとうございます。" | toot --visibility unlisted
 
